46fd32f9e168d3d24c9754f2000da588a2e03075
yulei <304189773@qq.com>
2015-06-03 11:00:02 +0800

    修改sql分隔符

--- src/main/resources/sql-install/Oracle/sdk.sql
+++ src/main/resources/sql-install/Oracle/sdk.sql
@@ -1,9 +1,9 @@
 INSERT INTO st_user (id, account, password, name, phone, email, enabled, logic_deleted)
-VALUES (1,  'admin', '',  '管理员', NULL, NULL, '0', '0')/
+VALUES (1,  'admin', '',  '管理员', NULL, NULL, '0', '0')^_^
 
 INSERT INTO st_group (id, parent_id, code, name, is_leaf, sort_no, type, logic_deleted)
-VALUES (1, -1, 'root', '组织结构', '1', 1, '', '0')/
+VALUES (1, -1, 'root', '组织结构', '1', 1, '', '0')^_^
 
 INSERT INTO st_group_user (id, user_id, group_id, is_default, sort_no)
-VALUES (0, 1, 1, '1', 1)/
+VALUES (0, 1, 1, '1', 1)^_^
  
